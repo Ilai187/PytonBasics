26d093ef93e653db8400e060571fbce78112bc2f
Ilai Schnueriger <ilai_schnueriger@sluz.ch>
2025-09-09 15:52:21 +0200

    Aufgabe 2 Fertiggestellt

--- task_1_2.py
+++ task_1_2.py
@@ -1,2 +1,10 @@
 # Block 1 Aufgabe 2
-print("hier ist aufgabe 2")
+
+print (type(10))
+print (type(10.00101010))
+print (type("Ilai"))
+
+
+#Unterschied zwischen = und ==
+#  = dient der Zuweisung.
+#  == dient dem Vergleich von Werten.
