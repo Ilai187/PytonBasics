32c53a8bfb91e3f10598039d6f5fab1a1610239d
Ilai Schnueriger <ilai_schnueriger@sluz.ch>
2025-09-09 15:44:26 +0200

    Aufgabe 1 Abgeschlossen

--- task_1_1.py
+++ task_1_1.py
@@ -1,4 +1,10 @@
 #Block 1 Aufgabe 1
-print("hallo")
-print("das ist mein erstes Programm")
-print("hier löse ich aufgabe 1")
+
+name = input("Wie heisst du? ")
+begrüssung = "Hallo, " + name + "! Schön, dich zu sehen."
+print(begrüssung)
+
+number1 = 12
+number2 = 10
+print(number1 + number2)
+
