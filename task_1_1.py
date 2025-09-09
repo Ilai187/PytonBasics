cbdeb4975074987d9f36cdba46988d2e1f23dc91
Ilai Schnueriger <ilai_schnueriger@sluz.ch>
2025-09-09 16:14:44 +0200

    Aufgabe 1 Abgeschlossen

--- task_1_1.py
+++ task_1_1.py
@@ -4,7 +4,7 @@ name = input("Wie heisst du? ")
 begrüssung = "Hallo, " + name + "! Schön, dich zu sehen."
 print(begrüssung)
 
-number1 = 12
-number2 = 10
-print(number1 + number2)
+zahl1 = int(input("Gib eine Zahl ein: "))
+zahl2 = int(input("Gib nochmal Zahl ein: "))
+print (zahl1 + zahl1)
 
