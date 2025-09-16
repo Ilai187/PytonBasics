3ccdb4afc7ccac8710665970165ad6ba571d6c90
Ilai Schnueriger <ilai_schnueriger@sluz.ch>
2025-09-16 15:20:17 +0200

    Aufgabe 4 Abgeschlossen

---
+++ task_1_4.py
@@ -0,0 +1,9 @@
+zahl1 = float(input("Gib eine Zahl ein: "))
+zahl2 = float(input("Gib eine zweite Zahl ein: "))
+
+print (round(zahl1 + zahl2,2))
+print (round(zahl1 - zahl2,2))
+print (round(zahl1 / zahl2,2))
+print (round(zahl1 * zahl2,2))
+
+# / gibt immer eine Kommazahl zurück, während // das Ergebnis auf die nächste ganze Zahl nach unten abrundet.
