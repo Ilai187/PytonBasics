e0b68b24597bc3a2293feedd48c06df465f3960e
Ilai Schnueriger <ilai_schnueriger@sluz.ch>
2025-09-16 15:11:25 +0200

    Aufgabe 3 Abgeschlossen

--- task_1_3.py
+++ task_1_3.py
@@ -1,3 +1,10 @@
 # Block 1 Aufgabe 3
 name = input("Gib bitte dein Namen ein: ")
-print(name)
+print(name.upper())
+print(name.lower())
+print(name[::-1])
+
+wort1= input("Gib ein Wort ein: ")
+wort2= input("Gib ein zweites Wort ein: ")
+print(wort1 + wort2)
+
