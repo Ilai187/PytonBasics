f936b7ce4a55ee2b88223c581c01996bf3eace06
Ilai Schnueriger <ilai_schnueriger@sluz.ch>
2025-09-23 15:06:35 +0200

    Aufgabe 10 Abgeschlossen

--- task_1_10.py
+++ task_1_10.py
@@ -1,1 +1,10 @@
-#
+distanz = float(input("Welche Distanz (KM) willst du auf deiner Reise zurücklegen? "))
+tempo = float(input("Wie Schnell wirst du durchschnittlich fahren? "))
+benzin = float(input("Wie hoch wird der Benzienverbrauch auf 100KM sein? "))
+fahrzeits = distanz / tempo
+fahrzeitm = fahrzeits * 60
+gesamtverbrauch = (distanz / 100) * benzin
+
+print(f"\nFahrzeit: {fahrzeitm:.1f} Minuten")
+print(f"Gesamtverbrauch: {gesamtverbrauch:.2f} Liter")
+
