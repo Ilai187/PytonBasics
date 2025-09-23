f6c6080eabd157537809c2a502c7222e5e4999ff
Ilai Schnueriger <ilai_schnueriger@sluz.ch>
2025-09-23 15:22:06 +0200

    Aufgabe 11 Abgeschlossen

--- task_1_11.py
+++ task_1_11.py
@@ -1,1 +1,8 @@
-#
+#BMI = Gewicht (kg) ÷ [Größe (m)]²
+
+grösse = float(input("Bitte gib deine Körpergrösse ein [Meter] "))
+gewicht = float(input("Bitte gib dein Körpergewicht ein [KG] "))
+
+bmi = gewicht/ (grösse ** 2)
+
+print(f"Dein BMI ist:", bmi)
