595b01cdf19f03883b4d94e2d23a56b0c02a9993
Ilai Schnueriger <ilai_schnueriger@sluz.ch>
2025-09-23 15:27:49 +0200

    Aufgabe 12 Abgeschlossen

--- task_1_12.py
+++ task_1_12.py
@@ -1,1 +1,6 @@
-#
+
+stunden = int(input("Gib Stunden ein: "))
+minuten = int(input("Gib Minuten ein: "))
+sekunden = int(input("Gib Sekunden ein: "))
+gesamtstunden = stunden + (minuten / 60) + (sekunden / 3600)
+print("Zeit in Stunden:", gesamtstunden)
