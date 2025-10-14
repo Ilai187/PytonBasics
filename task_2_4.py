72f5ea458e871203641091bc80fbcfd79f8e73fb
Ilai Schnueriger <ilai_schnueriger@sluz.ch>
2025-10-14 15:53:30 +0200

    Aufgabe 2.4 Fertiggestellt

--- task_2_4.py
+++ task_2_4.py
@@ -1,1 +1,25 @@
-#
+Geschlecht = input("Gib dein Geschlecht ein [m/w]: ").lower()
+Gewicht = int(input("Gib dein Gewicht ein [KG]: "))
+
+if Geschlecht == "m":
+    if Gewicht <= 55:
+        print("Gewichtsklasse: Fliegengewicht")
+elif Gewicht <= 66:
+    print("Gewichtsklasse: Leichtgewicht")
+elif Gewicht <= 84:
+    print("Gewichtsklasse: Mittelgewicht")
+else:
+    print("Gewichtsklasse: Schwergewicht")
+    
+if Geschlecht == "w":
+    if Gewicht <= 48:
+        print("Gewichtsklasse: Fliegengewicht")
+elif Gewicht <= 55:
+        print("Gewichtsklasse: Leichtgewicht")
+elif Gewicht <= 63:
+        print("Gewichtsklasse: Mittelgewicht")
+elif Gewicht <= 85:
+        print("Gewichtsklasse: Schwergewicht")
+
+else:
+    print("Ungültige Eingabe beim Geschlecht.")
