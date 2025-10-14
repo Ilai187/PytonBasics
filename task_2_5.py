56109b87110f4f5f37bc9ac780bfd549e3edf6ca
Ilai Schnueriger <ilai_schnueriger@sluz.ch>
2025-10-14 15:59:58 +0200

    Aufgabe 2.5 Fertiggestellt

--- task_2_5.py
+++ task_2_5.py
@@ -1,1 +1,12 @@
-#
+monat = int(input("Gib den aktuellen Monat ein [1-12]:"))
+if monat <= 3:
+    print ("Es ist Winter")
+elif monat <= 6:
+    print("Es ist frühling")
+elif monat <= 9:
+    print ("Es ist Sommer")
+elif monat <= 12: 
+    print ("Es ist Herbst")
+else:
+    print("Ungültige Zahl / Eingabe")
+    
